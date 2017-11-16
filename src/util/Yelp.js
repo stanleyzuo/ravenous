2d9b3b47c6f073b245600a2553c9cda57c8cebdd
stanleyzuo <sjrzuo@gmail.com>
2017-11-16 01:19:51 -0500

    first commit

---
+++ src/util/Yelp.js
@@ -0,0 +1,45 @@
+const clientID = 'IMw0IBVkVQyGWWjaHKrMxg';
+const secret = 'fbmNrOPWFYDwOXxJXvq4OjQSJrz7h5l5bmnrPCottACcepUCK3NY04gflN7ZzO6g';
+let accessToken = '';
+
+const Yelp = {
+	getAccessToken() {
+		if (accessToken) {
+			return new Promise(resolve =>
+				resolve(accessToken));
+		}
+		return fetch(`https://cors-anywhere.herokuapp.com/api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientID}&client_secret=${secret}`, 
+			{method:'POST'}).then(response => {
+				return response.json();
+			}).then(jsonResponse => {
+				accessToken = jsonResponse.access_token;
+			})
+	},
+	search(term, location, sortBy) {
+		return Yelp.getAccessToken().then(() => {
+			return fetch(`https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+				{headers:{Authorization:`Bearer ${accessToken}`}})
+		}).then(response => {
+			return response.json();
+		}).then(jsonResponse => {
+			if (jsonResponse.businesses) {
+				return jsonResponse.businesses.map(business => ({
+      
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }));
+        };
+      }
+    )
+	}
+}
+
+export default Yelp;
